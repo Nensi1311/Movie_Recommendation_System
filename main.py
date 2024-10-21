7ba973418053e263daf6bac7cbeb4aceec123b22
Nensi Pansuriya <155217979+Nensi1311@users.noreply.github.com>
2024-10-21 17:46:33 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,21 @@
 # import libraries
 import numpy as np
 import pandas as pd
+
+# data loading
+Movies = pd.read_csv("tmdb_5000_movies.csv")
+Movies
+credits = pd.read_csv("tmdb_5000_credits.csv")
+credits
+
+Movies.head()
+credits.head()
+
+Movies.describe()
+credits.describe()
+
+Movies.isnull().sum()
+credits.isnull().sum()
+
+Movies.info()
+credits.info()
