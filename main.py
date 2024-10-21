6343f2cfb37a162d11f8941923b7ccf7ce707ec9
Nensi Pansuriya <155217979+Nensi1311@users.noreply.github.com>
2024-10-21 18:02:43 +0530

    Update main.py

--- main.py
+++ main.py
@@ -26,8 +26,10 @@ movies.isnull().sum()
 movies["original_language"].value_counts()
 movies.drop(columns = ["budget", "homepage", "original_language", "original_title", "popularity", "production_companies", "production_countries", "release_date", "revenue", "runtime", "spoken_languages", "tagline", "vote_average", "vote_count", "id", "status"], axis = 0, inplace = True)
 movies.head()
+
 movies = movies[["movie_id", "title", "overview", "genres", "keywords", "cast", "crew"]]
 movies.head()
+
 movies.isnull().sum()
 movies.dropna(inplace = True)
 movies.isnull().sum()
