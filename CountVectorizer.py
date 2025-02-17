16657a6e83dbac2750ccdf2be3018f25eb334aa6
Nensi Pansuriya <155217979+Nensi1311@users.noreply.github.com>
2025-02-17 23:47:10 +0530

    Delete CountVectorizer.py

--- CountVectorizer.py
+++
@@ -1,9 +0,0 @@
-from sklearn.feature_extraction.text import CountVectorizer
-cv = CountVectorizer(max_features = 5000, stop_words = "english")
-
-vectors = cv.fit_transform(new_df["tags"]).toarray()
-vectors
-
-cv.fit_transform(new_df["tags"]).toarray().shape
-cv.get_feature_names_out()
-len(cv.get_feature_names_out())
