c00b7a617b242529dea3a9ca914c33964d31fad6
Nensi Pansuriya <155217979+Nensi1311@users.noreply.github.com>
2025-02-17 23:47:31 +0530

    Delete final.py

--- final.py
+++
@@ -1,9 +0,0 @@
-def recommend(movie):
-    movie_index = new_df[new_df["title"] == movie].index[0]
-    distance = similarity[movie_index]
-    movies_list = sorted(list(enumerate(distance)), reverse = True, key = lambda x : x[1])[1:6] 
-    
-    for i in movies_list:
-        print(new_df.iloc[i[0]].title)
-
-  recommend("Avatar")
